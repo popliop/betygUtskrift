60bc5b6da14348e6d5a05057b93fe16daf056548
Marcus <macansorm@hotmail.com>
2016-10-04 12:15:06 +0200

    test3

--- ElevHantare/src/com/marcus/main/Main.java
+++ ElevHantare/src/com/marcus/main/Main.java
@@ -17,7 +17,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		// init();
-		System.out.println("test2");
+		System.out.println("test3");
 		laddaElev("11");
 		nyElev("Marcus", 22);
 
